0bef5f7140e3bfe1bb1bc0457c00c46809342d5e
NoMoreBuild <woolimlee.dev@gmail.com>
2021-11-20 21:55:45 +0900

    update: 레이아웃, 메뉴 리팩토링

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
@@ -19,11 +19,34 @@ const HeaderWrapper = styled.div`
   }
 `
 
+// 메뉴 컨테이너
+const MenuContainer = styled.div`
+  // 너비, 높이, 위치 지정
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  // 메뉴 토글 애니메이션
+  transition: 0.8s all;
+  transform: ${props => (props.toggle ? "translateY(0)" : "translateY(-100%)")};
+  transform-origin: 100% 0;
+  // 배경
+  background-color: #f7f7f7;
+  // 정렬
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
 const MenuButton = styled.button`
   font-family: "Noto Serif KR", serif;
   border: none;
   background: none;
-  dispaly: inilne-block;
+  display: inilne-block;
   cursor: pointer;
   font-size: 1em;
 `
@@ -32,12 +55,63 @@ const StyledLink = styled(Link)`
   font-family: "Noto Serif KR", serif;
 `
 
-const Header = ({ toggleMenu }) => {
+// 메뉴 Link 스타일
+const MenuLink = styled(Link)`
+  @keyframes fade-in {
+    0%,
+    20% {
+      opacity: 0;
+      top: 50px;
+    }
+    80%,
+    100% {
+      opacity: 1;
+      top: 0px;
+    }
+  }
+
+  font-family: "Noto Serif KR", serif;
+  font-size: 2.986rem;
+  margin-bottom: 20px;
+  overflow: hidden;
+  display: inline;
+  position: relative;
+  animation: ${props =>
+    props.toggle === "true" ? `fade-in ${props.delay}s` : ""};
+  ${props => (props.toggle === "false" ? "opacity: 0" : "")};
+  transition: 0.4s opacity;
+  line-height: 1.2;
+`
+
+const Header = () => {
+  // 메뉴 토글 제어
+  const [toggle, setToggle] = useState(false)
+
+  const toggleMenu = () => {
+    setToggle(!toggle)
+  }
+
   return (
-    <HeaderWrapper>
-      <MenuButton onClick={toggleMenu}>메뉴</MenuButton>
-      <StyledLink to="/">홈</StyledLink>
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <MenuButton onClick={toggleMenu}>메뉴</MenuButton>
+        <StyledLink to="/">홈</StyledLink>
+      </HeaderWrapper>
+      <MenuContainer toggle={toggle}>
+        <MenuLink toggle={toggle.toString()} delay={1} to="/about-me">
+          자기소개
+        </MenuLink>
+        <MenuLink toggle={toggle.toString()} delay={1.2} to="/blog">
+          블로그
+        </MenuLink>
+        <MenuLink toggle={toggle.toString()} delay={1.4} to="/portfolio">
+          작업물
+        </MenuLink>
+        <MenuLink toggle={toggle.toString()} delay={1.6} to="/contact">
+          연락
+        </MenuLink>
+      </MenuContainer>
+    </>
   )
 }
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -36,6 +36,7 @@ const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Noto Serif KR', serif;
     margin-bottom: 20px;
+    line-height: 1.2;
   }
 
   h1 {
@@ -68,84 +69,13 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-// 메뉴 컨테이너
-const MenuContainer = styled.div`
-  // 너비, 높이, 위치 지정
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  // 메뉴 토글 애니메이션
-  transition: 0.8s all;
-  transform: ${props => (props.toggle ? "translateY(0)" : "translateY(-100%)")};
-  transform-origin: 100% 0;
-  // 배경
-  background-color: #f7f7f7;
-  // 정렬
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`
-
-// 메뉴 Link 스타일
-const MenuLink = styled(Link)`
-  @keyframes fade-in {
-    0%,
-    20% {
-      opacity: 0;
-      top: 50px;
-    }
-    80%,
-    100% {
-      opacity: 1;
-      top: 0px;
-    }
-  }
-
-  font-family: "Noto Serif KR", serif;
-  font-size: 2.986rem;
-  margin-bottom: 20px;
-  overflow: hidden;
-  display: inline;
-  position: relative;
-  animation: ${props =>
-    props.toggle === "true" ? `fade-in ${props.delay}s` : ""};
-  ${props => (props.toggle === "false" ? "opacity: 0" : "")};
-  transition: 0.4s opacity;
-`
-
 const Layout = ({ children }) => {
-  // 메뉴 토글 제어
-  const [toggle, setToggle] = useState(false)
-
-  const toggleMenu = () => {
-    setToggle(!toggle)
-  }
-
   return (
-    <>
+    <MainWrapper>
       <GlobalStyle />
-      <Header toggleMenu={toggleMenu} />
-      <MenuContainer toggle={toggle}>
-        <MenuLink toggle={toggle.toString()} delay={1} to="/about-me">
-          자기소개
-        </MenuLink>
-        <MenuLink toggle={toggle.toString()} delay={1.2} to="/blog">
-          블로그
-        </MenuLink>
-        <MenuLink toggle={toggle.toString()} delay={1.4} to="/portfolio">
-          작업물
-        </MenuLink>
-        <MenuLink toggle={toggle.toString()} delay={1.6} to="/contact">
-          연락
-        </MenuLink>
-      </MenuContainer>
-      <MainWrapper>{children}</MainWrapper>
-    </>
+      <Header />
+      {children}
+    </MainWrapper>
   )
 }
 
