d8513b5935544da755f81f81278edd66e805d42e
NoMoreBuild <woolimlee.dev@gmail.com>
2021-11-21 15:31:15 +0900

    update: 콘텐츠 영역 디바이스 별 너비 지정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,18 @@ const MainWrapper = styled.div`
   width: 100%;
   height: calc(100vh - 40px);
   padding-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 700px;
+
+  // 모바일 컨테이너 width 90%로 고정
+  @media only screen and (max-width: 768px) {
+    width: 90%;
+  }
+  // 해상도가 1440px 일 때 1280px로 고정
+  @media only screen and (min-width: 1440px) {
+    width: 1280px;
+  }
 `
 
 // GlobalStyles
@@ -30,6 +42,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Noto Serif KR', serif;
     margin-bottom: 20px;
     line-height: 1.3;
+    word-break: keep-all;
   }
 
   h1 {
