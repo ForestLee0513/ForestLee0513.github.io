d0be5423a3fb49f4ac84d1ac536357e1037c2fbd
NoMoreBuild <woolimlee.dev@gmail.com>
2021-12-07 18:17:12 +0900

    update: 가로 리스트 기기에 따라 scroll event 적용 분기 추가

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,17 @@
 import React, { useRef, useEffect, useState, useCallback } from "react"
 import styled, { css } from "styled-components"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { Link } from "gatsby"
+import { isBrowser } from "react-device-detect"
 import { debounce } from "../utils/debounce"
 
 const ListContainer = styled.ul`
-  overflow-x: hidden;
   margin-left: calc(50% - 50vw);
   margin-right: calc(50% - 50vw);
   list-style: none;
   flex: 1;
   display: flex;
   align-items: center;
+  overflow-x: ${props => (props.$isBrowser ? "hidden" : "scroll")};
 
   * {
     ${props => props.$isScroll && "pointer-events: none;"}
@@ -57,9 +57,10 @@ const ListItem = styled.li`
 
 // 포트폴리오용 리스트 랜더링
 const List = ({ items }) => {
-  const listRef = useRef()
-  const [isScroll, setIsScroll] = useState(false)
+  const listRef = useRef() // 리스트 컨테이너 ref (ref로 지정 안하면 event target이 변경돼서 scrollLeft 사용 불가)
+  const [isScroll, setIsScroll] = useState(false) // 스크롤 여부
 
+  // 가로 휠 스크롤
   const horizontalScroll = useCallback(event => {
     event.preventDefault()
     // 스크롤 위치 변경
@@ -73,8 +74,12 @@ const List = ({ items }) => {
   }, [])
 
   useEffect(() => {
-    listRef.current?.addEventListener("wheel", horizontalScroll)
+    // 브라우저 일때만 커스텀 스크롤 이벤트 적용
+    if (isBrowser) {
+      listRef.current?.addEventListener("wheel", horizontalScroll)
+    }
 
+    // cleanup
     return () => {
       setIsScroll(false)
       listRef.current = null
@@ -82,32 +87,29 @@ const List = ({ items }) => {
   }, [horizontalScroll])
 
   return (
-    <ListContainer ref={listRef} $isScroll={isScroll}>
+    <ListContainer ref={listRef} $isScroll={isScroll} $isBrowser={isBrowser}>
       {items.map((item, index) => {
-        const title = item.frontmatter.title
-        const thumbnail = item.frontmatter.thumbnail
-        const date = item.frontmatter.date
-        const formattedNumber = index + 1 < 10 ? `0${index + 1}` : index + 1
-        const remarkedThumbnail = getImage(thumbnail)
+        const title = item.frontmatter.title // 제목
+        const thumbnail = getImage(item.frontmatter.thumbnail) // 썸네일
+        const date = item.frontmatter.date // 날짜
+        const formattedNumber = index + 1 < 10 ? `0${index + 1}` : index + 1 // 포트폴리오 번호 (1번부터 시작 / 10 이하일때 앞에 0 추가)
 
         return (
           <ListItem key={index}>
-            <Link to="/">
-              <h4>{formattedNumber}</h4>
-              <GatsbyImage
-                image={remarkedThumbnail}
-                alt={title}
-                css={css`
-                  display: block !important;
-                  min-width: 200px;
-                  max-width: 200px;
-                  min-height: 250px;
-                  max-height: 250px;
-                `}
-              />
-              <h3>{title}</h3>
-              <p>{date}</p>
-            </Link>
+            <h4>{formattedNumber}</h4>
+            <GatsbyImage
+              image={thumbnail}
+              alt={title}
+              css={css`
+                display: block !important;
+                min-width: 200px;
+                max-width: 200px;
+                min-height: 250px;
+                max-height: 250px;
+              `}
+            />
+            <h3>{title}</h3>
+            <p>{date}</p>
           </ListItem>
         )
       })}
